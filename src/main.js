47a8d47e7e67c5a914d0eb9767818220680cf814
Santiago Sanchez Hernandez <122324891+garbanzo12@users.noreply.github.com>
2026-02-04 14:05:48 -0500

    Creación de microservico CecoName
    Este microservicio fue creado en base a otro microservicio llamado Companies-MicroService

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+dotenv.config();
+
+import express from 'express';
+import { poolPromise } from './infrastructure/database/typeorm/mssql-pool.js';
+import { CecoNameRepositoryImpl } from './infrastructure/database/typeorm/repositories/CecoNameRepositoryImpl.js';
+import { GetCecoNames } from './application/use-cases/GetCecoNames.js';
+import { GetCecoNameById } from './application/use-cases//GetCecoNameById.js';
+import {CecoNameController} from './infrastructure/http/controllers/CecoNameController.js'
+const app = express();
+app.use(express.json());
+// app.use(morgan('dev')); <-- Usar morgan en caso de necesitar observar los tiempos de respuesta, en controllers ya se imprimen respuestas de petición
+const pool = await poolPromise; // Conexion a la pool mssql  
+
+const repo = new CecoNameRepositoryImpl(pool);
+const getCecoNameUseCase = new GetCecoNames(repo);
+const getCecoNameByIdUseCase = new GetCecoNameById(repo);
+const CecoameController = new CecoNameController(getCecoNameUseCase, getCecoNameByIdUseCase);
+
+//Endpoints de companies 
+app.get('/ceconame', (req, res) => CecoameController.getAll(req, res));
+app.get('/ceconame/:id', (req, res) => CecoameController.getById(req, res));
+
+app.listen(3002, () => {
+    console.log('🚀 Microservicio de Compañías (ODBC Nativo) en puerto 3002');
+});
